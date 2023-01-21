a75d09ca0f60b068494509809d2addcee0c4da94
Arik Alexandrov <arikxl@gmail.com>
2023-01-21 02:08:17 +0200

    deploy22

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
       <footer className="w-full flex justify-center items-center
          bg-[#55efc4] sm:px-8 px-4 py-2.5 border-b border-b-[#e6ebf4]">
-          <p>
+          <p className="text-white">
               Created with human intelligence by
               <a href='https://www.linkedin.com/in/arik-alexandrov/' target="_blank">arikxl {new Date().getFullYear}</a>
           </p>
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,11 +3,13 @@ import FormField from './FormField';
 import Loader from './Loader';
 import CardList from './CardList';    
 import CardItem from './CardItem';    
+import Footer from './Footer';    
 
 export {
     Header,
     FormField,
     Loader,
     CardList,
-    CardItem
+    CardItem,
+    Footer
 };
